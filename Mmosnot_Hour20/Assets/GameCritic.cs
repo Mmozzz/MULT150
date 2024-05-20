c0aaea2b4d22648d2341fb959a540d54c25a7136
Mmozzz <161980688+Mmozzz@users.noreply.github.com>
2024-05-19 23:34:54 -0700

    Mmosnot_Hour20

---
+++ Mmosnot_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,12 @@
+/*
+1. Easy:It's a simple game with simple Mechanics. You only have to dodge one obstacle at a time.
+   Hard:The movement isn’t the best, and it's hard to know if you lined up with the powerup when you're on the sides. 
+2. Add more than one obstacle at a time. Adding more movement to the character.
+3. Make the environment look better and add obstacles that you would need to slide under or jump over. 
+4. Trying to get a high time and trying to avoid as many obstacles as possible.
+5. Lining up the powerups and being slowed down when you get hit since it just doesn’t feel like a smooth transition. 
+
+ Changes:
+1. Made the obstacles bigger and made to look like the lazers on the side.
+2. Changed the Powerups shape, glows more, and added a rainbow effect for the particles.
+ */
